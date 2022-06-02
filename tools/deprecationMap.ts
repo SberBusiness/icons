4e0f324f993d6c4e41a66adf51337578e159a07f
svgaryaev <svgaryaev@gmail.com>
2022-06-02 16:49:04 +0300

    Синхронизация иконок с фигмой

---
+++ tools/deprecationMap.ts
@@ -0,0 +1,21 @@
+/**
+ * Мапа deprecated иконок, значение означает иконку-заменитель,
+ * если значение не указано, значит иконка безнадежно deprecated.
+ */
+export const deprecationMap = {
+    ic_prd_account_default_32_w: '',
+    ic_prd_banknote_default_32_w: '',
+    ic_prd_banktax_default_20_w: '',
+    ic_prd_counterpartypay_default_20_w: '',
+    ic_prd_homeservice_default_20_w: '',
+    ic_srv_answer_default_20_w: '',
+    ic_srv_review_default_20_w: '',
+    ic_prd_mailbell_default_32_w: 'ic_nav_mailbell_default_32_w',
+    ic_srvx_balance_default_24_w: 'ic_nav_balance_default_24_w',
+    ic_prd_changeaccount_default_20_w: 'ic_nav_changeaccount_default_20_w',
+    ic_srvx_changeaccount_default_24_w: 'ic_nav_changeaccount_default_24_w',
+    ic_srvx_notification_default_24_w: 'ic_nav_notification_default_24_w',
+    ic_srvx_notificationnew_default_24_w: 'ic_nav_notificationnew_default_24_w',
+    ic_srvx_exit_default_24_w: 'ic_nav_exit_default_24_w',
+    ic_srvx_settingsberraiting_default_24_w: 'ic_nav_settingsberraiting_default_24_w',
+};
