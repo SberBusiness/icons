1b2dd85670aee6e3227f76cb2b6c6eb6443538ad
Amory lorch Romny weaver <aromny-w@student.21-school.ru>
2024-09-16 12:11:34 +0300

    Добавление иконок и иллюстраций

--- tools/deprecationMap.ts
+++ tools/deprecationMap.ts
@@ -4,5 +4,7 @@
  */
 export const deprecationMap = {
     ic_srvx_listsorting_default_24_lm_w: 'ic_srvx_listsortingoff_default_24_lm_w',
-    ic_nav_barchart_default_16_lm_w: 'ic_nav_barchartvertical_default_16_lm_w'
+    ic_nav_barchart_default_16_lm_w: 'ic_nav_barchartvertical_default_16_lm_w',
+    ic_prd_sbermarketactual_default_20_lm_w: 'ic_prd_kuper_default_20_lm_w',
+    ic_prd_sbermarketactual_default_32_lm_w: 'ic_prd_kuper_default_32_lm_w'
 };
