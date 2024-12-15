8b0b5b00d2b06fb1805724955f8df3391bb9f1c3
Amory lorch Romny weaver <aromny-w@student.21-school.ru>
2024-12-16 00:34:18 +0300

    Добавление иконок

--- tools/deprecationMap.ts
+++ tools/deprecationMap.ts
@@ -1,10 +1,12 @@
 /**
- * Мапа deprecated иконок, значение означает иконку-заменитель,
- * если значение не указано, значит иконка безнадежно deprecated.
+ * Мапа устаревших иконок, где значение означает иконку-заменитель.
+ * Если значение не указано, значит иконка безнадежно устарела.
  */
 export const deprecationMap = {
     ic_srvx_listsorting_default_24_lm_w: 'ic_srvx_listsortingoff_default_24_lm_w',
     ic_nav_barchart_default_16_lm_w: 'ic_nav_barchartvertical_default_16_lm_w',
     ic_prd_sbermarketactual_default_20_lm_w: 'ic_prd_kuper_default_20_lm_w',
-    ic_prd_sbermarketactual_default_32_lm_w: 'ic_prd_kuper_default_32_lm_w'
+    ic_prd_sbermarketactual_default_32_lm_w: 'ic_prd_kuper_default_32_lm_w',
+    ic_brd_tinkoff_default_24_lm_w: 'ic_brd_tbank_default_24_lm_w',
+    ic_brd_homecreditbank_default_24_lm_w: 'ic_brd_homebank_default_24_lm_w'
 };
