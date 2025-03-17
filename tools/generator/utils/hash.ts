421364addafcd5312f7c1735de21bc8be61b66a0
Amory lorch Romny weaver <aromny-w@student.21-school.ru>
2025-03-17 15:14:24 +0300

    Изменение хеш-функции

--- tools/generator/utils/hash.ts
+++ tools/generator/utils/hash.ts
@@ -1,74 +1,79 @@
-/** Алфавит для кодировок в base64. */
-const b64a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-';
+const fmix32 = (h: number) => {
+    h ^= h >>> 16;
+    h = Math.imul(h, 0x85ebca6b);
+    h ^= h >>> 13;
+    h = Math.imul(h, 0xc2b2ae35);
+    h ^= h >>> 16;
+
+    return h;
+};
+
+const rotl32 = (x: number, r: number) => (x << r) | (x >>> (32 - r));
 
 /**
- * Преобразует массив байт в base64, используется для создания хэшей для css-классов на основе массива байт.
- * Не добавляет конечные знаки "=", так как для css-хэшей они не нужны.
+ * Простая и быстрая хеш-функция общего назначения, разработанная Остином Эпплби.
+ * Не является криптографически-безопасной, возвращает 32-разрядное беззнаковое число.
  *
- * @param ba Входящий массив байт.
- * @returns Base64-представление массива без дополнения конечными знаками "=".
+ * @param key Строка для хеширования.
+ * @param len Длина строки.
+ * @param seed Стартовое число.
+ * @return Хэш в виде числа.
  */
-const ba2b64 = (ba: number[]): string => {
-    let a = 0,
-        b = 0,
-        c = 0,
-        result = '',
-        i = 0,
-        n = ba.length,
-        m = n % 3;
+const murmurhash3 = (key: string, len: number, seed: number) => {
+    let h1 = seed;
+    let k1: number;
 
-    n -= m;
-    while (i < n) {
-        a = ba[i++];
-        b = ba[i++];
-        c = ba[i++];
-        result += b64a[a >> 2] + b64a[((a << 4) & 63) | (b >> 4)] + b64a[((b << 2) & 63) | (c >> 6)] + b64a[c & 63];
+    const c1 = 0xcc9e2d51;
+    const c2 = 0x1b873593;
+
+    const remainder = len & 3;
+    const bytes = len - remainder;
+
+    let i = 0;
+
+    while (i < bytes) {
+        k1 =
+            (key.charCodeAt(i++) & 0xff) |
+            ((key.charCodeAt(i++) & 0xff) << 8) |
+            ((key.charCodeAt(i++) & 0xff) << 16) |
+            ((key.charCodeAt(i++) & 0xff) << 24);
+
+        k1 = Math.imul(k1, c1);
+        k1 = rotl32(k1, 15);
+        k1 = Math.imul(k1, c2);
+
+        h1 ^= k1;
+        h1 = rotl32(h1, 13);
+        h1 = Math.imul(h1, 5) + 0xe6546b64;
     }
 
-    if (m === 1) {
-        a = ba[i];
-        result += b64a[a >> 2] + b64a[(a << 4) & 63];
-    } else if (m === 2) {
-        a = ba[i++];
-        b = ba[i];
-        result += b64a[a >> 2] + b64a[((a << 4) & 63) | (b >> 4)] + b64a[(b << 2) & 63];
+    k1 = 0;
+
+    switch (remainder) {
+        case 3:
+            k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16;
+        case 2:
+            k1 ^= (key.charCodeAt(i + 1) & 0xff) << 8;
+        case 1:
+            k1 ^= key.charCodeAt(i) & 0xff;
+
+            k1 = Math.imul(k1, c1);
+            k1 = rotl32(k1, 15);
+            k1 = Math.imul(k1, c2);
+            h1 ^= k1;
     }
-    return result;
+
+    h1 ^= len;
+
+    h1 = fmix32(h1);
+
+    return h1 >>> 0;
 };
 
 /**
- * Реализация простого и достаточно смешанного алгоритма Adler23. Здесь используется для генерации хэшей css-классов.
- * От SHA отказался, так как от этого SHA-хэша используется только кусок, а Adler32-хэш в шесть символов base64 влезает весь
- * и при этом стоек к коллизиям при длине строки до 256 символов.
- *
- * @param array Массив байт для хэширования.
- * @return Хэш в виде массива байт.
+ * Основная функция для хеширования имени класса.
+ * Подчеркивание необходимо, т.к. в CSS имя не может начинаться с цифры.
  */
-const adler32 = (array: number[]): number[] => {
-    let s1 = 1,
-        s2 = 0;
-
-    for (let i = 0, l = array.length; i < l; i++) {
-        s1 = (s1 + array[i]) % 65521;
-        s2 = (s2 + s1) % 65521;
-    }
-
-    return [(s2 >> 16) & 0xff, s2 & 0xff, (s1 >> 16) & 0xff, s1 & 0xff];
+export const hash = (key: string, seed = 0): string => {
+    return '_' + murmurhash3(key, key.length, seed).toString(36);
 };
-
-/**
- * Функция перевода строки в массив байт (не поддерживает unicode).
- */
-const str2ba = (str: string): number[] => {
-    let out = [];
-    let p = 0;
-    for (let i = 0, n = str.length; i < n; i++) {
-        out[p++] = str.charCodeAt(i);
-    }
-    return out;
-};
-
-/**
- * Хэширующая функция, используемая для создания base64-хэшей имён классов css.
- */
-export const hash = (str: string): string => ba2b64(adler32(str2ba(str)));
